1bee41e867fb95d15752117d4543190b92dbdaed
Amit0458 <amitkumar51997@gmail.com>
2022-12-15 17:53:25 +0530

    data push

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ server.use(express.static("public"));
 
 // config
 const protectedRoutes = [
-  { route: "/users", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+  { route: "/users", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
   { route: "/posts", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-  { route: "/comments", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+  { route: "/workoutplan", methods: ["POST", "PUT", "DELETE", "PATCH"] },
   { route: "/photos", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-  { route: "/todos", methods: ["POST", "PUT", "DELETE", "PATCH"] },
+  { route: "/routine", methods: ["POST", "PUT", "DELETE", "PATCH"] },
   { route: "/recipeCategories", methods: ["POST", "PUT", "DELETE", "PATCH"] },
   { route: "/recipeIngredients", methods: ["POST", "PUT", "DELETE", "PATCH"] },
   { route: "/recipes", methods: ["POST", "PUT", "DELETE", "PATCH"] },
